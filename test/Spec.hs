8f3d92f6048fd9178f25afc0f5a7c02dfaa3b975
Vytenis Narmontas <vyt.narmontas@gmail.com>
2024-12-09 18:56:44 +0200

    Initial

---
+++ test/Spec.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+import Test.Tasty ( TestTree, defaultMain, testGroup )
+import Test.Tasty.HUnit ( testCase, (@?=) )
+import Test.Tasty.QuickCheck as QC
+
+import Data.List
+import Data.Ord
+
+import Lib1 qualified
+import Lib2 qualified
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests, propertyTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Lib1 tests"
+  [ testCase "List of completions is not empty" $
+      null Lib1.completions @?= False,
+    testCase "Parsing case 1 - give a better name" $
+      Lib2.parseQuery "" @?= (Left "Some error message"),
+    testCase "Parsing case 2 - give a better name" $
+      Lib2.parseQuery "o" @?= (Left "Some error message")
+  ]
+
+propertyTests :: TestTree
+propertyTests = testGroup "some meaningful name"
+  [
+    QC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
+  ]
